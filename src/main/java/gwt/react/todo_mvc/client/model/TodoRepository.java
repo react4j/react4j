ea308b2a73636e1373b8a2d073ffcc012116b773
Peter Donald <peter@realityforge.org>
2017-09-25 14:51:53 +1000

    Add more code to ease the ability to generate this class

--- src/main/java/gwt/react/todo_mvc/client/model/TodoRepository.java
+++ src/main/java/gwt/react/todo_mvc/client/model/TodoRepository.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.realityforge.arez.Arez;
 import org.realityforge.arez.Disposable;
 import org.realityforge.arez.Observable;
@@ -16,11 +17,11 @@ public class TodoRepository
 {
   private final Observable _observable =
     Arez.context().createObservable( Arez.context().areNamesEnabled() ? "Todos.todo" : null );
-  private final HashMap<Object,Todo> _entities = new HashMap<>();
-  private final Collection<Todo> _entityList = Collections.unmodifiableCollection( _entities.values());
+  private final HashMap<Object, Todo> _entities = new HashMap<>();
+  private final Collection<Todo> _entityList = Collections.unmodifiableCollection( _entities.values() );
 
   @Action
-  public void create( @Nonnull final String id, @Nonnull final String title, final boolean completed )
+  public final void create( @Nonnull final String id, @Nonnull final String title, final boolean completed )
   {
     final Arez_Todo entity = new Arez_Todo( id, title, completed );
     _entities.put( entity.getId(), entity );
@@ -28,8 +29,15 @@ public class TodoRepository
   }
 
   @Action
-  public void destroy( @Nonnull final Todo todo )
+  public final boolean contains( @Nonnull final Todo todo )
   {
+    return _entities.containsKey( todo.getId() );
+  }
+
+  @Action
+  public final void destroy( @Nonnull final Todo todo )
+  {
+    //TODO: Should this next line be an invariant check
     assert null != todo;
     if ( null != _entities.remove( todo.getId() ) )
     {
@@ -38,12 +46,28 @@ public class TodoRepository
       // unless of course todo is not disposable
       _observable.reportChanged();
     }
+    else
+    {
+      //TODO: fail an apiInvariant check?
+    }
+  }
+
+  @Nullable
+  public final Todo findById( @Nonnull final String id )
+  {
+    return _entities.get( id );
   }
 
   @Nonnull
-  public Collection<Todo> findAll()
+  public final Collection<Todo> findAll()
   {
     _observable.reportObserved();
     return _entityList;
+  }
+
+  @Nonnull
+  public final TodoRepository self()
+  {
+    return this;
   }
 }
