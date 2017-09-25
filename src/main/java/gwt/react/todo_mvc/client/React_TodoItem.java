b3102560d787bb296d2872766af0c41a603f9889
Peter Donald <peter@realityforge.org>
2017-09-25 10:46:14 +1000

    Optimise imports

--- src/main/java/gwt/react/todo_mvc/client/React_TodoItem.java
+++ src/main/java/gwt/react/todo_mvc/client/React_TodoItem.java
@@ -1,6 +1,5 @@
 package gwt.react.todo_mvc.client;
 
-import gwt.react.client.GwtReactConfig;
 import gwt.react.client.components.Component;
 import gwt.react.client.elements.ReactElement;
 import gwt.react.todo_mvc.client.TodoItem.Props;
@@ -8,9 +7,6 @@ import gwt.react.todo_mvc.client.TodoItem.State;
 import javax.annotation.Nonnull;
 import jsinterop.annotations.JsConstructor;
 import jsinterop.annotations.JsType;
-import jsinterop.base.JsConstructorFn;
-import jsinterop.base.JsPropertyMap;
-import org.realityforge.braincheck.Guards;
 
 /**
  * TODO: This should be generated from annotation processor, processing TodoItem.
