848b534060389ba2ad69dd5fa5ced3fbfb4fe6a9
Peter Donald <peter@realityforge.org>
2017-09-25 10:46:06 +1000

    Remove Person model and associated play model

--- src/main/java/gwt/react/todo_mvc/client/TodoList.java
+++ src/main/java/gwt/react/todo_mvc/client/TodoList.java
@@ -171,8 +171,7 @@ class TodoList
                 ),
                 renderMainSection(),
                 renderFooter( activeTodoCount, completedCount )
-           ),
-           React.createElement( PersonComponent_.TYPE, PersonComponent.Props.create( 1 ) )
+           )
       );
   }
 
