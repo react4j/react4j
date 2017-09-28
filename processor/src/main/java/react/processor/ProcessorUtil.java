7f1e744eadba8387ff989d54c27f33e5c0277cc5
Peter Donald <peter@realityforge.org>
2017-09-28 14:50:31 +1000

    Remove some unused methods

--- processor/src/main/java/react/processor/ProcessorUtil.java
+++ processor/src/main/java/react/processor/ProcessorUtil.java
@@ -1,23 +1,13 @@
 package react.processor;
 
-import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.ParameterSpec;
-import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 import com.squareup.javapoet.TypeVariableName;
-import java.lang.annotation.Documented;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import javax.lang.model.AnnotatedConstruct;
-import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
@@ -35,17 +25,6 @@ final class ProcessorUtil
 
   private ProcessorUtil()
   {
-  }
-
-  @Nonnull
-  static List<TypeVariableName> getTypeArgumentsAsNames( @Nonnull final DeclaredType declaredType )
-  {
-    final List<TypeVariableName> variables = new ArrayList<>();
-    for ( final TypeMirror argument : declaredType.getTypeArguments() )
-    {
-      variables.add( TypeVariableName.get( (TypeVariable) argument ) );
-    }
-    return variables;
   }
 
   @Nonnull
@@ -78,15 +57,6 @@ final class ProcessorUtil
     }
   }
 
-  @Nonnull
-  static List<ExecutableElement> getConstructors( @Nonnull final TypeElement element )
-  {
-    return element.getEnclosedElements().stream().
-      filter( m -> m.getKind() == ElementKind.CONSTRUCTOR ).
-      map( m -> (ExecutableElement) m ).
-      collect( Collectors.toList() );
-  }
-
   static void copyAccessModifiers( @Nonnull final TypeElement element, @Nonnull final TypeSpec.Builder builder )
   {
     if ( element.getModifiers().contains( Modifier.PUBLIC ) )
@@ -95,83 +65,11 @@ final class ProcessorUtil
     }
   }
 
-  static void copyAccessModifiers( @Nonnull final ExecutableElement element, @Nonnull final MethodSpec.Builder builder )
-  {
-    if ( element.getModifiers().contains( Modifier.PUBLIC ) )
-    {
-      builder.addModifiers( Modifier.PUBLIC );
-    }
-    else if ( element.getModifiers().contains( Modifier.PROTECTED ) )
-    {
-      builder.addModifiers( Modifier.PROTECTED );
-    }
-  }
-
-  static void copyDocumentedAnnotations( @Nonnull final AnnotatedConstruct element,
-                                         @Nonnull final MethodSpec.Builder builder )
-  {
-    for ( final AnnotationMirror annotation : element.getAnnotationMirrors() )
-    {
-      final DeclaredType annotationType = annotation.getAnnotationType();
-      if ( !annotationType.toString().startsWith( "org.realityforge.arez.annotations." ) &&
-           null != annotationType.asElement().getAnnotation( Documented.class ) )
-      {
-        builder.addAnnotation( AnnotationSpec.get( annotation ) );
-      }
-    }
-  }
-
-  static void copyDocumentedAnnotations( @Nonnull final AnnotatedConstruct element,
-                                         @Nonnull final ParameterSpec.Builder builder )
-  {
-    for ( final AnnotationMirror annotation : element.getAnnotationMirrors() )
-    {
-      if ( null != annotation.getAnnotationType().asElement().getAnnotation( Documented.class ) )
-      {
-        builder.addAnnotation( AnnotationSpec.get( annotation ) );
-      }
-    }
-  }
-
-  static void copyExceptions( @Nonnull final ExecutableType method, @Nonnull final MethodSpec.Builder builder )
-  {
-    for ( final TypeMirror thrownType : method.getThrownTypes() )
-    {
-      builder.addException( TypeName.get( thrownType ) );
-    }
-  }
-
   static void copyTypeParameters( @Nonnull final ExecutableType action, @Nonnull final MethodSpec.Builder builder )
   {
     for ( final TypeVariable typeParameter : action.getTypeVariables() )
     {
       builder.addTypeVariable( TypeVariableName.get( typeParameter ) );
-    }
-  }
-
-  @Nullable
-  static String deriveName( @Nonnull final ExecutableElement method,
-                            @Nonnull final Pattern pattern,
-                            @Nonnull final String name )
-    throws ReactProcessorException
-  {
-    if ( isSentinelName( name ) )
-    {
-      final String methodName = method.getSimpleName().toString();
-      final Matcher matcher = pattern.matcher( methodName );
-      if ( matcher.find() )
-      {
-        final String candidate = matcher.group( 1 );
-        return Character.toLowerCase( candidate.charAt( 0 ) ) + candidate.substring( 1 );
-      }
-      else
-      {
-        return null;
-      }
-    }
-    else
-    {
-      return name;
     }
   }
 
