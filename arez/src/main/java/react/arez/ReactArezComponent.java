da23bcbc6cf16695f4c760abdbca6938173b6186
Peter Donald <peter@realityforge.org>
2017-10-02 09:37:18 +1100

    Update Arez version to 0.03

--- arez/src/main/java/react/arez/ReactArezComponent.java
+++ arez/src/main/java/react/arez/ReactArezComponent.java
@@ -38,7 +38,7 @@ public abstract class ReactArezComponent<P extends BaseProps, S extends BaseStat
     final ArezContext context = Arez.context();
     _propsObservable = context.createObservable( toName( ".props" ) );
     _stateObservable = context.createObservable( toName( ".state" ) );
-    _renderReaction = context.reaction( toName( ".reaction" ), false, this::onRenderDepsChanged );
+    _renderReaction = context.tracker( toName( ".reaction" ), false, this::onRenderDepsChanged );
   }
 
   private void onRenderDepsChanged()
