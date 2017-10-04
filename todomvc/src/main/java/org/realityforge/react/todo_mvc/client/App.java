99c7ff7158c63b8ab92a8475d1d7a06ffe831fb3
Peter Donald <peter@realityforge.org>
2017-10-04 14:30:30 +1100

    Decruft

--- todomvc/src/main/java/org/realityforge/react/todo_mvc/client/App.java
+++ todomvc/src/main/java/org/realityforge/react/todo_mvc/client/App.java
@@ -3,7 +3,6 @@ package org.realityforge.react.todo_mvc.client;
 import com.google.gwt.core.client.EntryPoint;
 import elemental2.dom.DomGlobal;
 import org.realityforge.arez.Arez;
-import org.realityforge.arez.extras.WhyRun;
 import react.arez.ReactArezConsoleSpyEventProcessor;
 import react.core.React;
 import react.dom.ReactDOM;
@@ -13,11 +12,6 @@ public class App
 {
   static final int ESCAPE_KEY = 27;
   static final int ENTER_KEY = 13;
-
-  static void whyRun()
-  {
-    DomGlobal.console.log( new WhyRun( Arez.context() ).whyRun() );
-  }
 
   private static void spyEvents()
   {
