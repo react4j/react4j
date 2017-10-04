1fd6aca7a0448627ef5d807fd7896b2d644134e1
Peter Donald <peter@realityforge.org>
2017-10-04 17:04:42 +1100

    Add enum that documents the component phases

---
+++ core/src/main/java/react4j/core/ComponentPhase.java
@@ -0,0 +1,51 @@
+package react4j.core;
+
+/**
+ * This indicates the major lifecycle phase that the component is going through.
+ * These match those in the <a href="https://reactjs.org/docs/react-component.html">React Component</a>
+ * API with the addition of {@link #INITIALIZING} that the Java API needs. This constant is only used
+ * during validation of state and should be optimized away in production builds.
+ */
+enum ComponentPhase
+{
+  /**
+   * The beginning of the components lifecycle.
+   * The component goes through the steps:
+   * <ul>
+   * <li>Constructed.</li>
+   * <li>Bound to a native react component via {@link Component#bindComponent(NativeComponent)}.</li>
+   * <li>{@link Component#componentInitialize()} is invoked.</li>
+   * </ul>
+   */
+  INITIALIZING,
+  /**
+   * The component is rendered initially and mounted on the dom.
+   * The component goes through the steps:
+   * <ul>
+   * <li>{@link Component#componentWillMount()} is invoked.</li>
+   * <li>{@link Component#render()} is invoked.</li>
+   * <li>{@link Component#componentDidMount()} is invoked.</li>
+   * </ul>
+   */
+  MOUNTING,
+  /**
+   * The component is potentially re-rendered due to updates.
+   * The component goes through the steps:
+   * <ul>
+   * <li>{@link Component#componentWillReceiveProps(BaseProps)} if props were provided. i.e. the parent component was re-rendered.</li>
+   * <li>{@link Component#shouldComponentUpdate(BaseProps, BaseState)} and if the component returns false the update will be aborted.</li>
+   * <li>{@link Component#componentWillUpdate(BaseProps, BaseState)}.</li>
+   * <li>{@link Component#render()}.</li>
+   * <li>{@link Component#componentDidUpdate(BaseProps, BaseState)}.</li>
+   * </ul>
+   */
+  UPDATING,
+  /**
+   * The component is being removed from the dom.
+   * There is one step:
+   * <ul>
+   * <li>{@link Component#componentWillUnmount()}.</li>
+   * </ul>
+   */
+  UNMOUNTING
+}
