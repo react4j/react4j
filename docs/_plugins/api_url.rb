864e0946f0d9fd032221d9b55f775f148ada9ba9
Peter Donald <peter@realityforge.org>
2017-10-07 15:30:31 +1100

    Add new plugin to help writing documentation

---
+++ docs/_plugins/api_url.rb
@@ -0,0 +1,22 @@
+module Jekyll
+  #
+  # A simple tag for generating url for api documentation.
+  #
+  # {% api_url core:ArezContext %}
+  #
+  class ApiUrl < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      markup = markup.strip
+      @classname = markup
+
+      super
+    end
+
+    def render(context)
+      classname = "react4j.#{@classname}"
+      return "#{context['site']['baseurl']}/api/#{classname.gsub('.','/')}.html"
+    end
+  end
+end
+
+Liquid::Template.register_tag('api_url', Jekyll::ApiUrl)
