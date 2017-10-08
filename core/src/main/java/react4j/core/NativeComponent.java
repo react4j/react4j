8425d211123558a90c070b3dfa3546ec86c79315
Peter Donald <peter@realityforge.org>
2017-10-08 15:07:47 +1100

    Remove class from public API by making it package access

--- core/src/main/java/react4j/core/NativeComponent.java
+++ core/src/main/java/react4j/core/NativeComponent.java
@@ -1,25 +1,4 @@
 package react4j.core;
-/* The MIT License (MIT)
-
-Copyright (c) 2016 GWT React
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE. */
 
 import java.util.Objects;
 import javax.annotation.Nonnull;
@@ -32,44 +11,30 @@ import jsinterop.base.JsPropertyMap;
 
 /**
  * The react native component.
+ * This class is not part of the public API and is purely an implementation detail.
  *
  * @param <P> the type of props this component expects
  * @param <S> the type of state this component maintains
  */
 @JsType( isNative = true, namespace = "React", name = "Component" )
-public abstract class NativeComponent<P extends BaseProps, S extends BaseState>
+abstract class NativeComponent<P extends BaseProps, S extends BaseState>
 {
-  @JsProperty
-  private S state;
-
   @SuppressWarnings( "unused" )
   @JsProperty
   private P props;
-
+  @JsProperty
+  private S state;
   @SuppressWarnings( "unused" )
   @JsProperty
   private JsPropertyMap<Object> refs;
 
-  protected NativeComponent( @Nonnull final P props )
+  NativeComponent( @SuppressWarnings( "unused" ) @Nonnull final P props )
   {
   }
-
-  native void setState( @Nonnull S state );
-
-  native void setState( @Nonnull Component.SetStateCallback<P, S> callback );
-
-  native void forceUpdate();
 
   @JsMethod
   @Nullable
   protected abstract ReactElement<?, ?> render();
-
-  @JsOverlay
-  @Nullable
-  final S state()
-  {
-    return state;
-  }
 
   @JsOverlay
   @Nullable
@@ -79,10 +44,23 @@ public abstract class NativeComponent<P extends BaseProps, S extends BaseState>
   }
 
   @JsOverlay
+  @Nullable
+  final S state()
+  {
+    return state;
+  }
+
+  final native void setState( @Nonnull S state );
+
+  final native void setState( @Nonnull Component.SetStateCallback<P, S> callback );
+
+  @JsOverlay
   final void setInitialState( @Nonnull final S state )
   {
     this.state = Objects.requireNonNull( state );
   }
+
+  final native void forceUpdate();
 
   @JsOverlay
   final JsPropertyMap<Object> refs()
