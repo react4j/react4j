b92680ee9b9365653205614a72fbe94fe40af823
Peter Donald <peter@realityforge.org>
2017-10-08 14:58:39 +1100

    optimise imports

--- todomvc/src/main/java/react4j/todomvc/TodoList.java
+++ todomvc/src/main/java/react4j/todomvc/TodoList.java
@@ -6,10 +6,6 @@ import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import jsinterop.base.Js;
 import org.realityforge.arez.annotations.ArezComponent;
-import react4j.todomvc.Footer_;
-import react4j.todomvc.TodoEntry_;
-import react4j.todomvc.TodoItem_;
-import react4j.todomvc.model.AppData;
 import react4j.annotations.EventHandler;
 import react4j.annotations.ReactComponent;
 import react4j.arez.ReactArezComponent;
@@ -23,8 +19,9 @@ import react4j.dom.events.MouseEventHandler;
 import react4j.dom.proptypes.html.HtmlProps;
 import react4j.dom.proptypes.html.InputProps;
 import react4j.dom.proptypes.html.attributeTypes.InputType;
-import static react4j.todomvc.TodoList_.*;
+import react4j.todomvc.model.AppData;
 import static react4j.dom.DOM.*;
+import static react4j.todomvc.TodoList_.*;
 
 @ReactComponent
 @ArezComponent( allowEmpty = true )
