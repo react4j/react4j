808f7cc772ff219d68dafb4f1fe090ba21546bac
Peter Donald <peter@realityforge.org>
2017-10-08 14:45:35 +1100

    Remove unused method

--- core/src/main/java/react4j/core/BaseState.java
+++ core/src/main/java/react4j/core/BaseState.java
@@ -1,11 +1,7 @@
 package react4j.core;
 
-import elemental2.core.JsObject;
-import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
-import jsinterop.base.Js;
-import jsinterop.base.JsPropertyMap;
 
 /**
  * Base class for state field.
@@ -13,10 +9,4 @@ import jsinterop.base.JsPropertyMap;
 @JsType( isNative = true, namespace = JsPackage.GLOBAL, name = "Object" )
 public class BaseState
 {
-  @JsOverlay
-  public final <T> T dup()
-  {
-    return Js.uncheckedCast( JsObject.assign( Js.uncheckedCast( JsPropertyMap.of() ),
-                                              Js.uncheckedCast( JsPropertyMap.of( this ) ) ) );
-  }
 }
