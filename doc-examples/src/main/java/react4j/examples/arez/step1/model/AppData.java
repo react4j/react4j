86620a236462233fce7d5f88aa15ba70f4f30b09
Peter Donald <peter@realityforge.org>
2017-10-26 22:13:19 +1100

    Fix compilation error

--- doc-examples/src/main/java/react4j/examples/arez/step1/model/AppData.java
+++ doc-examples/src/main/java/react4j/examples/arez/step1/model/AppData.java
@@ -7,7 +7,7 @@ public final class AppData
   public static final BrowserLocation location = BrowserLocation.create();
   public static final TodoRepository model = TodoRepository.newRepository();
   public static final TodoService service = new Arez_TodoService( model );
-  public static final ViewService viewService = new Arez_ViewService( location );
+  public static final ViewService viewService = new Arez_ViewService();
 
   private AppData()
   {
