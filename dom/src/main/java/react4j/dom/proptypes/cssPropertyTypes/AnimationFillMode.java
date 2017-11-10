cee0f4910a3209bf88a2df166e3a89f4dc21c216
Peter Donald <peter@realityforge.org>
2017-11-10 21:05:46 +1100

    Decruft

--- dom/src/main/java/react4j/dom/proptypes/cssPropertyTypes/AnimationFillMode.java
+++ dom/src/main/java/react4j/dom/proptypes/cssPropertyTypes/AnimationFillMode.java
@@ -1,8 +1,5 @@
 package react4j.dom.proptypes.cssPropertyTypes;
 
-/**
- * Created by paul on 2017-04-09.
- */
 public enum AnimationFillMode
 {
   none, forwards, backwards, both, initial, inherit;
