a869ead3b0c66323ee5e9b2c67893c2f9d768833
Peter Donald <peter@realityforge.org>
2017-12-12 13:25:24 +1100

    Remove unused field

--- processor/src/main/java/react4j/processor/Constants.java
+++ processor/src/main/java/react4j/processor/Constants.java
@@ -7,7 +7,6 @@ final class Constants
   static final String DAGGER_PROVIDES_CLASSNAME = "dagger.Provides";
   static final String JS_FUNCTION_CLASSNAME = "jsinterop.annotations.JsFunction";
   static final String COMPONENT_CLASSNAME = "react4j.core.Component";
-  static final String PROCEDURE_CLASSNAME = "react4j.core.Procedure";
   static final String EVENT_HANDLER_ANNOTATION_CLASSNAME = "react4j.annotations.EventHandler";
   static final String REACT_COMPONENT_ANNOTATION_CLASSNAME = "react4j.annotations.ReactComponent";
   static final String INJECT_ANNOTATION_CLASSNAME = "javax.inject.Inject";
