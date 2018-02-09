a9d8887aba8e87e29edd50871cf8a5b5e56b4e2e
Peter Donald <peter@realityforge.org>
2018-02-09 11:45:47 +1100

    Remove some unused fields

--- processor/src/main/java/react4j/processor/Constants.java
+++ processor/src/main/java/react4j/processor/Constants.java
@@ -6,12 +6,10 @@ final class Constants
   static final String DAGGER_MODULE_CLASSNAME = "dagger.Module";
   static final String DAGGER_PROVIDES_CLASSNAME = "dagger.Provides";
   static final String JS_FUNCTION_CLASSNAME = "jsinterop.annotations.JsFunction";
-  static final String JS_PROPERTY_MAP_CLASSNAME = "jsinterop.base.JsPropertyMap";
   static final String COMPONENT_CLASSNAME = "react4j.core.Component";
   static final String EVENT_HANDLER_ANNOTATION_CLASSNAME = "react4j.annotations.EventHandler";
   static final String PROP_ANNOTATION_CLASSNAME = "react4j.annotations.Prop";
   static final String PROP_DEFAULT_ANNOTATION_CLASSNAME = "react4j.annotations.PropDefault";
-  static final String NO_AUTO_ACTION_ANNOTATION_CLASSNAME = "react4j.arez.NoAutoAction";
   static final String REACT_COMPONENT_ANNOTATION_CLASSNAME = "react4j.annotations.ReactComponent";
   static final String INJECT_ANNOTATION_CLASSNAME = "javax.inject.Inject";
   static final String AUTORUN_ANNOTATION_CLASSNAME = "arez.annotations.Autorun";
