eed7440f1d4744ac21af001e7e23c5a561e2cb43
Peter Donald <peter@realityforge.org>
2018-06-04 15:05:36 +1000

    Move some functionality into parent class

--- downstream-test/src/test/java/react4j/downstream/AbstractDownstreamTest.java
+++ downstream-test/src/test/java/react4j/downstream/AbstractDownstreamTest.java
@@ -4,11 +4,13 @@ import gir.sys.SystemProperty;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
+import org.realityforge.gwt.symbolmap.SymbolEntryIndex;
 
 public abstract class AbstractDownstreamTest
 {
@@ -52,5 +54,28 @@ public abstract class AbstractDownstreamTest
       .get( SystemProperty.get( "react4j.deploy_test.work_dir" ) )
       .toAbsolutePath()
       .normalize();
+  }
+
+  @Nonnull
+  protected final SymbolEntryIndex getSymbolMapIndex( @Nonnull final Path archiveDir, @Nonnull final String build )
+    throws Exception
+  {
+    return SymbolEntryIndex.readSymbolMapIntoIndex( getSymbolMapPath( archiveDir, build ) );
+  }
+
+  @Nonnull
+  private Path getSymbolMapPath( @Nonnull final Path archiveDir, @Nonnull final String build )
+    throws IOException
+  {
+    final Path symbolMapsDir =
+      archiveDir
+        .resolve( build )
+        .resolve( "assets" )
+        .resolve( "WEB-INF" )
+        .resolve( "deploy" )
+        .resolve( "todomvc" )
+        .resolve( "symbolMaps" );
+
+    return Files.list( symbolMapsDir ).findFirst().orElseThrow( AssertionError::new );
   }
 }
--- downstream-test/src/test/java/react4j/downstream/BuildOutputTest.java
+++ downstream-test/src/test/java/react4j/downstream/BuildOutputTest.java
@@ -1,9 +1,5 @@
 package react4j.downstream;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import javax.annotation.Nonnull;
 import org.realityforge.gwt.symbolmap.SymbolEntryIndex;
 import org.testng.annotations.Test;
 import react4j.gwt.qa.React4jBuildAsserts;
@@ -15,26 +11,7 @@ public class BuildOutputTest
   public void rawProduction()
     throws Exception
   {
-    final String build = "raw.after";
-
-    final SymbolEntryIndex index = SymbolEntryIndex.readSymbolMapIntoIndex( getSymbolMapPath( build ) );
-
+    final SymbolEntryIndex index = getSymbolMapIndex( getArchiveDir(), "raw.after" );
     React4jBuildAsserts.assertReact4jOutputs( index, false );
-  }
-
-  @Nonnull
-  private Path getSymbolMapPath( @Nonnull final String build )
-    throws IOException
-  {
-    final Path symbolMapsDir =
-      getArchiveDir()
-        .resolve( build )
-        .resolve( "assets" )
-        .resolve( "WEB-INF" )
-        .resolve( "deploy" )
-        .resolve( "todomvc" )
-        .resolve( "symbolMaps" );
-
-    return Files.list( symbolMapsDir ).findFirst().orElseThrow( AssertionError::new );
   }
 }
