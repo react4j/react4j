c4351862671b932777219e4e580c5bcaf696198f
Peter Donald <peter@realityforge.org>
2018-07-19 16:26:22 +1000

    Name symbol appropriately.

--- core/src/main/java/react4j/ReactConfig.native.js
+++ core/src/main/java/react4j/ReactConfig.native.js
@@ -1,3 +1,3 @@
 // Ensure that @defines are included so that they can specified at compile time for the closure compiler
 /** @suppress {extraRequire} */
-let jsinterop = goog.require('react4j');
+let react4j = goog.require('react4j');
