841eb2fcf7e7350c548758e9bd193a878ad7a125
Peter Donald <peter@realityforge.org>
2018-07-19 14:54:56 +1000

    Add @defines for the closure compiler

---
+++ arez/src/main/java/react4j/arez/ReactArezConfig.native.js
@@ -0,0 +1,3 @@
+// Ensure that @defines are included so that they can specified at compile time for the closure compiler
+/** @suppress {extraRequire} */
+let jsinterop = goog.require('react4j.arez');
---
+++ arez/src/main/java/react4j/arez/react4j.js
@@ -0,0 +1,8 @@
+/**
+ * This file provides the @defines for react4j configuration options.
+ * See ReactArezConfig.java for details.
+ */
+goog.provide('react4j.arez');
+
+/** @define {string} */
+goog.define('react4j.arez.store_arez_data_as_state', 'false');
