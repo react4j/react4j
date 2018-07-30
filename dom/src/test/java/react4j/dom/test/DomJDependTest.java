ecf0f166c39f8c03468a7a1a148f2b74a774b13b
Peter Donald <peter@realityforge.org>
2018-07-31 06:46:40 +1000

    Add some additional dependencies

--- dom/src/test/java/react4j/dom/test/DomJDependTest.java
+++ dom/src/test/java/react4j/dom/test/DomJDependTest.java
@@ -37,7 +37,9 @@ public class DomJDependTest
 
     react4jDom.dependsUpon( react4j );
     react4jDom.dependsUpon( jsinteropAnnotations );
+    react4jDom.dependsUpon( jsinteropBase );
     react4jDom.dependsUpon( braincheck );
+    react4jDom.dependsUpon( elemental2Core );
     react4jDom.dependsUpon( elemental2Dom );
     react4jDom.dependsUpon( react4jDomHtml );
 
