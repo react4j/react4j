f57e21b0cc40813d26805633f48b43265cfc814d
Peter Donald <peter@realityforge.org>
2018-11-06 21:32:10 +1100

    Add another magic prop key

--- core/src/main/java/react4j/PropNames.java
+++ core/src/main/java/react4j/PropNames.java
@@ -2,6 +2,7 @@ package react4j;
 
 final class PropNames
 {
+  static final String CHILDREN_PROP_NAME = "children";
   static final String KEY_PROP_NAME = "key";
   static final String REF_PROP_NAME = "ref";
 
