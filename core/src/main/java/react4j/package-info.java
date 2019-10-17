c1859ac4f95ccc2d809d58bf6fe7aefa31b69263
Peter Donald <peter@realityforge.org>
2019-10-17 15:21:21 +1100

    Remove some redundent escapes

--- core/src/main/java/react4j/package-info.java
+++ core/src/main/java/react4j/package-info.java
@@ -1,10 +1,10 @@
 /**
  * Core React4j API.
  */
-@OmitPattern( type = "^react4j\\.[^\\.]+$", symbol = "^\\$clinit$" )
+@OmitPattern( type = "^react4j\\.[^.]+$", symbol = "^\\$clinit$" )
 // Make sure the generated classes do not have clinits unless required
-@OmitPattern( type = "^.*\\.React4j_[^\\.]+$", symbol = "^\\$clinit$" )
-@KeepPattern( type = "^.*\\.React4j_[^\\.]+\\$Factory$", symbol = "^\\$clinit$" )
+@OmitPattern( type = "^.*\\.React4j_[^.]+$", symbol = "^\\$clinit$" )
+@KeepPattern( type = "^.*\\.React4j_[^.]+\\$Factory$", symbol = "^\\$clinit$" )
 package react4j;
 
 import grim.annotations.KeepPattern;
