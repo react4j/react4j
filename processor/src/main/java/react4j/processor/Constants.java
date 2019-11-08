87ade40932c8210d25c9c792f30194eafcafdf88
Peter Donald <peter@realityforge.org>
2019-11-08 11:41:33 +1100

    Whitespace

--- processor/src/main/java/react4j/processor/Constants.java
+++ processor/src/main/java/react4j/processor/Constants.java
@@ -34,8 +34,8 @@ final class Constants
   static final String NULLABLE_ANNOTATION_CLASSNAME = "javax.annotation.Nullable";
   static final String DEPRECATED_ANNOTATION_CLASSNAME = "java.lang.Deprecated";
   static final String JS_ERROR_CLASSNAME = "elemental2.core.JsError";
-
-  static final String MUTABLE_PROP_ACCESSED_IN_POST_CONSTRUCT_SUPPRESSION = "React4j:MutablePropAccessedInPostConstruct";
+  static final String MUTABLE_PROP_ACCESSED_IN_POST_CONSTRUCT_SUPPRESSION =
+    "React4j:MutablePropAccessedInPostConstruct";
 
   private Constants()
   {
