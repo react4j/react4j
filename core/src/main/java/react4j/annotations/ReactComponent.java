2344ef7e0cdfbf9134fcdc38f1cc8fd191edf6a2
Peter Donald <peter@realityforge.org>
2020-02-12 23:00:58 +1100

    Add initial StingProvider annotation

--- core/src/main/java/react4j/annotations/ReactComponent.java
+++ core/src/main/java/react4j/annotations/ReactComponent.java
@@ -12,6 +12,7 @@ import javax.annotation.Nonnull;
  */
 @Documented
 @Target( ElementType.TYPE )
+@StingProvider( "[FlatEnclosingName][SimpleName]Factory" )
 public @interface ReactComponent
 {
   /**
