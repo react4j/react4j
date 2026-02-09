c8b9433e9b181a7fc70ff407f7a74b1b053477dd
Peter Donald <peter@realityforge.org>
2026-02-09 18:51:26 +1100

    ALign error message

--- processor/src/main/java/react4j/processor/React4jProcessor.java
+++ processor/src/main/java/react4j/processor/React4jProcessor.java
@@ -1272,10 +1272,10 @@ public final class React4jProcessor
                                                   Constants.SUPPRESS_REACT4J_WARNINGS_CLASSNAME ) )
         {
           final String message =
-            MemberChecks.shouldNot( Constants.RENDER_CLASSNAME,
-                                    "should be annotated by a @Nonnull or a @Nullable annotation. " +
-                                    MemberChecks.suppressedBy( Constants.WARNING_MISSING_RENDER_NULLABILITY,
-                                                               Constants.SUPPRESS_REACT4J_WARNINGS_CLASSNAME ) );
+            MemberChecks.should( Constants.RENDER_CLASSNAME,
+                                 "be annotated by a @Nonnull or a @Nullable annotation. " +
+                                 MemberChecks.suppressedBy( Constants.WARNING_MISSING_RENDER_NULLABILITY,
+                                                            Constants.SUPPRESS_REACT4J_WARNINGS_CLASSNAME ) );
           processingEnv.getMessager().printMessage( Diagnostic.Kind.WARNING, message, method );
         }
 
