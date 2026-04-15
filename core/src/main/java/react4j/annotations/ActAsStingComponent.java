0ad9b6782b48c6fe27bbb39413698b5e0d44e366
Peter Donald <peter@realityforge.org>
2026-04-15 16:36:16 +1000

    Grammar

--- core/src/main/java/react4j/annotations/ActAsStingComponent.java
+++ core/src/main/java/react4j/annotations/ActAsStingComponent.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Integration into sting the name of the annotation and shape is all the matters.
+ * Integration into sting the name of the annotation and shape is all that matters.
  */
 @Retention( RetentionPolicy.RUNTIME )
 @Target( ElementType.ANNOTATION_TYPE )
