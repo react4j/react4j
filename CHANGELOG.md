95b597ee2dbd866cdee50e1ab3d0765b557e5a9d
Peter Donald <peter@realityforge.org>
2026-09-22 19:23:14 +1000

    Fix legacy closure namespace configuration.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ### Unreleased
 
+* Fix legacy closure namespace configuration.
+
 ### [v0.229](https://github.com/react4j/react4j/tree/v0.229) (2026-09-11) · [Full Changelog](https://github.com/react4j/react4j/compare/v0.228...v0.229)
 
 Changes in this release:
--- core/src/main/java/react4j/react4j.js
+++ core/src/main/java/react4j/react4j.js
@@ -8,19 +8,19 @@ goog.require('jre');
 
 /** @define {string} */
 react4j.environment = goog.define('react4j.environment', 'production');
-jre.addSystemPropertyFromGoogDefine('react4j.environment', react4j.environment);
+goog.module.get('jre').addSystemPropertyFromGoogDefine('react4j.environment', react4j.environment);
 /** @define {string} */
 react4j.enable_component_names = goog.define('react4j.enable_view_names', 'false');
-jre.addSystemPropertyFromGoogDefine('react4j.enable_view_names', react4j.enable_component_names);
+goog.module.get('jre').addSystemPropertyFromGoogDefine('react4j.enable_view_names', react4j.enable_component_names);
 /** @define {string} */
 react4j.check_invariants = goog.define('react4j.check_invariants', 'false');
-jre.addSystemPropertyFromGoogDefine('react4j.check_invariants', react4j.check_invariants);
+goog.module.get('jre').addSystemPropertyFromGoogDefine('react4j.check_invariants', react4j.check_invariants);
 /** @define {string} */
 react4j.minimize_input_keys = goog.define('react4j.minimize_input_keys', 'true');
-jre.addSystemPropertyFromGoogDefine('react4j.minimize_input_keys', react4j.minimize_input_keys);
+goog.module.get('jre').addSystemPropertyFromGoogDefine('react4j.minimize_input_keys', react4j.minimize_input_keys);
 /** @define {string} */
 react4j.validate_input_values = goog.define('react4j.validate_input_values', 'false');
-jre.addSystemPropertyFromGoogDefine('react4j.validate_input_values', react4j.validate_input_values);
+goog.module.get('jre').addSystemPropertyFromGoogDefine('react4j.validate_input_values', react4j.validate_input_values);
 /** @define {string} */
 react4j.store_debug_data_as_state = goog.define('react4j.store_debug_data_as_state', 'false');
-jre.addSystemPropertyFromGoogDefine('react4j.store_debug_data_as_state', react4j.store_debug_data_as_state);
+goog.module.get('jre').addSystemPropertyFromGoogDefine('react4j.store_debug_data_as_state', react4j.store_debug_data_as_state);
